dc3b47a053ae63dd5a55d86fea3e975f5f41332c
Oleh Bondaruk <oleh.bondaruk@synapse.com>
2016-07-15 08:26:13 +0300

    Right folder?

---
+++ ZaycevMavenProject/src/test/java/ZaycevMaven/ZaycevMavenProject/ValidSignIn_Test.java
@@ -0,0 +1,38 @@
+package ZaycevMaven.ZaycevMavenProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ValidSignIn_Test {
+
+	WebDriver driver;
+	ZaycevHomePage zaycevHomePage;
+	
+	@Before
+	public void setUp(){
+		driver = new FirefoxDriver();
+		zaycevHomePage = new ZaycevHomePage(driver);
+	}
+	
+	@After
+	public void tearDown(){
+		zaycevHomePage.closeBrowser();
+	}
+	
+	@Test
+	public void testValidSignIn(){
+		zaycevHomePage.openZaycevHomePage();
+		zaycevHomePage.clickEnterButton();
+		zaycevHomePage.enterEmail("Bondaruk9000@gmail.com");
+		zaycevHomePage.enterPassword("532757575");
+		zaycevHomePage.clickEntryButton();
+		assertTrue(zaycevHomePage.checkThatUserEntered().contains("Uncle_Bond"));
+	}
+	
+
+}
